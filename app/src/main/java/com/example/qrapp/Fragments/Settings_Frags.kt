1dbeb57bfba013562dc75a0ce6dec89eaedd3fbf
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-03-05 22:22:36 +0530

    committed

--- app/src/main/java/com/example/qrapp/Fragments/Settings_Frags.kt
+++ app/src/main/java/com/example/qrapp/Fragments/Settings_Frags.kt
@@ -7,7 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.Switch
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.appcompat.widget.SwitchCompat
 import com.example.qrapp.R
 
 
@@ -23,6 +28,17 @@ class Settings_Frags : Fragment() {
         val view =  inflater.inflate(R.layout.fragment_settings__frags, container, false)
         val blueBox = view.findViewById<ImageView>(R.id.blue_box_drop_container)
         val titleText = view.findViewById<TextView>(R.id.titleName)
+        val switchButton = view.findViewById<SwitchCompat>(R.id.switcToDark)
+
+        switchButton.setOnCheckedChangeListener{ buttonView, isChecked ->
+            if(isChecked){
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+            } else{
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+            }
+
+        }
+
 
         val topAnim = AnimationUtils.loadAnimation(context,R.anim.top_anim)
         blueBox.startAnimation(topAnim)
