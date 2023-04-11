fa0e17b2015666fd1515b1e8d7900b0b649713b0
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-11 14:44:28 +0530

    committed

--- app/src/main/java/com/example/qrapp/Fragments/Settings_Frags.kt
+++ app/src/main/java/com/example/qrapp/Fragments/Settings_Frags.kt
@@ -1,5 +1,6 @@
 package com.example.qrapp.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Switch
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
@@ -14,7 +16,7 @@ import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import androidx.appcompat.widget.SwitchCompat
 import com.example.qrapp.R
-
+import com.example.qrapp.aboutApp
 
 
 class Settings_Frags : Fragment() {
@@ -28,15 +30,12 @@ class Settings_Frags : Fragment() {
         val view =  inflater.inflate(R.layout.fragment_settings__frags, container, false)
         val blueBox = view.findViewById<ImageView>(R.id.blue_box_drop_container)
         val titleText = view.findViewById<TextView>(R.id.titleName)
-        val switchButton = view.findViewById<SwitchCompat>(R.id.switcToDark)
 
-        switchButton.setOnCheckedChangeListener{ buttonView, isChecked ->
-            if(isChecked){
-                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
-            } else{
-                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
-            }
-
+//        val aboutTab : TextView = view.findViewById(R.id.aboutTv)
+        val aboutTab : LinearLayout = view.findViewById(R.id.linearTouch)
+        aboutTab.setOnClickListener {
+            val toAboutApp = Intent(context,aboutApp::class.java)
+            startActivity(toAboutApp)
         }
 
 
